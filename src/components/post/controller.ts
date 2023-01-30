0cd1d54237c8281919a13f49e5389cdd5090915d
tineodev <tineo.dev@gmail.com>
2023-01-30 17:43:04 -0500

    Add method get-user-post

--- src/components/post/controller.ts
+++ src/components/post/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { okTrue, okFalse } from "../../responses";
+import { connect } from "http2";
 
 const prisma = new PrismaClient();
 
@@ -23,11 +24,29 @@ export const getIDPost = async (
   res: Response
 ): Promise<Response> => {
   try {
+    const idURL = req.params.id;
+
+    const element = await prisma.post.findUnique({
+      where: { id: Number(idURL) },
+      include: { user: true },
+    });
+
+    return okTrue({ res, result: element, message: `Posts ${idURL}` });
+  } catch (error) {
+    return okFalse({ res, message: error });
+  }
+};
+
+export const getUserPost = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
     const urlID = req.params.id;
 
     const element = await prisma.post.findMany({
       where: { userID: Number(urlID) },
-      include: { tags: true, user: true },
+      include: { tags: true },
     });
 
     return okTrue({ res, result: element, message: "ID posts" });
@@ -75,6 +94,50 @@ export const postPost = async (
         message: "Post created",
       });
     }
+  } catch (error) {
+    return okFalse({ res, message: error });
+  }
+};
+
+export const putPost = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const idURL = req.params.id;
+    const data = req.body;
+
+    const element = await prisma.post.update({
+      where: { id: Number(idURL) },
+      include: { tags: true },
+      data: {
+        title: data.title,
+        body: data.body,
+        tags: { connect: { id: data.tag_id } },
+      },
+    });
+
+    return okTrue({ res, result: element, message: "Post updated" });
+  } catch (error) {
+    console.log(error);
+    return okFalse({ res, message: error });
+  }
+};
+
+export const deletePost = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  try {
+    const idURL = req.params.id;
+
+    const element = await prisma.post.delete({
+      where: {
+        id: Number(idURL),
+      },
+    });
+
+    return okTrue({ res, result: element, message: "Post deleted" });
   } catch (error) {
     return okFalse({ res, message: error });
   }
--- src/components/post/index.ts
+++ src/components/post/index.ts
@@ -4,6 +4,9 @@ const routerPost = Router();
 
 routerPost.get("/", Posts.getPost);
 routerPost.get("/:id", Posts.getIDPost);
+routerPost.get("/user/:id", Posts.getUserPost);
 routerPost.post("/", Posts.postPost);
+// routerPost.put("/:id", Posts.putPost);
+routerPost.delete("/:id", Posts.deletePost);
 
 export default routerPost;
