421e78595e8457c1319f01d0cf6efe0eefb89e3a
tineodev <tineo.dev@gmail.com>
2023-01-31 02:30:00 -0500

    Add POST, PUT, DELETE Comments

--- src/components/comment/index.ts
+++ src/components/comment/index.ts
@@ -5,8 +5,8 @@ const routerComment = Router();
 
 routerComment.get("/user/:id", Comment.getUserComment);
 routerComment.get("/post/:id", Comment.getPostComment);
-routerComment.post("/");
-routerComment.put("/");
-routerComment.delete("/");
+routerComment.post("/", Comment.postComment);
+routerComment.put("/:id", Comment.putComment);
+routerComment.delete("/:id", Comment.deleteComment);
 
 export default routerComment;
